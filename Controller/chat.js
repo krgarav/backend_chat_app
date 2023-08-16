600b239c0442e121e163af807e01684d84239b68
krgarav <krgarav@gmail.com>
2023-08-16 16:25:13 +0530

    Optimised the Chat App

--- Controller/chat.js
+++ Controller/chat.js
@@ -1,3 +1,5 @@
+const Chats = require("../Models/chat");
+const Sequelize = require("sequelize");
 exports.postMessage = async (req, res) => {
     try {
         const enteredMessage = req.body.message;
@@ -5,6 +7,37 @@ exports.postMessage = async (req, res) => {
         res.status(200).json({ message: "Message stored successfully" });
     } catch (err) {
         res.status(500).json({ message: "Message storation failed" });
+        console.log(err)
+    }
+}
+
+exports.getMessage = async (req, res) => {
+    const lastMessageId = +req.query.lastMessageId;
+
+    try {
+        if (lastMessageId > 1) {
+            const pageSize = 10; // Number of results per page
+            const totalRows = await Chats.count();
+            const offset = Math.max(totalRows - pageSize, 0); // Ensure offset is not negative
+            const chats = await Chats.findAll({
+                limit: pageSize,
+                offset: offset,
+            });
+            res.status(200).json({ chats: chats });
+        } else {
+            const chats = await Chats.findAll({
+                where: {
+                    id: {
+                        [Sequelize.Op.gt]: lastMessageId
+                    }
+                },
+                order: [['id', 'ASC']],
+                limit: 100 // Adjust the limit to your preferred batch size
+            });
+            req.status(200).json({ chats: chats })
+        }
+    } catch (err) {
+        res.status(404).json({ message: "data not found" });
         console.log(err)
     }
 }
--- Routes/chat.js
+++ Routes/chat.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const chatController = require("../Controller/chat")
 const authenticate = require("../Middleware/auth");
 router.post("/add-message",authenticate,chatController.postMessage);
-
+router.get("/get-message",authenticate,chatController.getMessage);
 
 module.exports = router;

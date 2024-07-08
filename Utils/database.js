ef2568d72b7967abe04d3009a1992ad7f7b21181
krgarav <krgarav@gmail.com>
2024-07-08 17:04:19 +0530

    changed configuration of db

--- Utils/database.js
+++ Utils/database.js
@@ -1,9 +1,27 @@
 const dotenv = require("dotenv");
 dotenv.config();
-
+const path = require("path");
+const fs = require('fs');
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    dialect: "mysql",
-    host: process.env.DB_HOST || "localhost"
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+//     dialect: "mysql",
+//     host: process.env.DB_HOST || "localhost"
+// });
+
+const caCertPath = path.join(__dirname, "../", "ca.pem");
+const caCert = fs.readFileSync(caCertPath);
+
+// Sequelize Configuration
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DBUSERNAME, process.env.DBPASSWORD, {
+    host: process.env.DBHOSTNAME,
+    dialect: 'mysql',
+    port: 26370,
+    dialectOptions: {
+        ssl: {
+            ca: caCert
+        }
+    }
 });
+
+
 module.exports = sequelize;

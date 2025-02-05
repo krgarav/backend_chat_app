647d0c9943f01d9e5c4514d512ed40f38e9eb70b
Kumar Gaurav <76562785+krgarav@users.noreply.github.com>
2025-02-05 12:05:09 +0530

    local ip api integrated

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ const Grouptable = require("./Models/grouptable");
 const UserGroup = require("./Models/usergroup");
 const app = express();
 const server = http.createServer(app);
-const cron = require('node-cron');
+const cron = require("node-cron");
+const os = require("os");
+
+const interfaces = os.networkInterfaces();
+
 dotenv.config();
 
 // Initialize Socket.IO server and listen for connections
@@ -60,22 +64,35 @@ app.use(express.urlencoded());
 app.use("/user/", authRoute);
 app.use(chatRoute);
 app.use(express.static(path.join(__dirname, "dist")));
-
+app.get("/localIP", (req, res) => {
+  const ips = [];
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === "IPv4" && !iface.internal) {
+        ips.push(iface.address);
+        // console.log(`Local IP: ${iface.address}`);
+      }
+    }
+  }
+  res.json({ localIPs: ips });
+});
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-
 // Define your cron job
-cron.schedule('*/5 * * * *', () => {
-    // This function runs every 30 seconds
-    console.log('Running a task every 30 seconds');
-    // You can call an endpoint or perform a task here
-    // Example: Call the /cronServer endpoint programmatically
-    const axios = require('axios');
-    axios.get('https://test2-render.onrender.com/cronServer')
-        .then(response => console.log(response.data))
-        .catch(error => console.error('Error calling /cronServer:', error.message));
+cron.schedule("*/5 * * * *", () => {
+  // This function runs every 30 seconds
+  console.log("Running a task every 30 seconds");
+  // You can call an endpoint or perform a task here
+  // Example: Call the /cronServer endpoint programmatically
+  const axios = require("axios");
+  axios
+    .get("https://test2-render.onrender.com/cronServer")
+    .then((response) => console.log(response.data))
+    .catch((error) =>
+      console.error("Error calling /cronServer:", error.message)
+    );
 });
 
 User.hasMany(Chat);

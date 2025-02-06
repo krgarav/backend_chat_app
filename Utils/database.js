e23f37347deaac64e0984fe5ef8af85fbe69f174
Kumar Gaurav <76562785+krgarav@users.noreply.github.com>
2025-02-06 16:43:23 +0530

    database configuration changed

--- Utils/database.js
+++ Utils/database.js
@@ -7,25 +7,25 @@ const Sequelize = require('sequelize');
 
 
 dotenv.config();
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DBUSERNAME, process.env.DBPASSWORD, {
-    dialect: "mysql",
-    host: process.env.DB_HOST || "localhost"
-});
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DBUSERNAME, process.env.DBPASSWORD, {
+//     dialect: "mysql",
+//     host: process.env.DB_HOST || "localhost"
+// });
 
 // const caCertPath = path.join(__dirname, "../", "ca.pem");
 // const caCert = fs.readFileSync(caCertPath);
 
-// // Sequelize Configuration
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DBUSERNAME, process.env.DBPASSWORD, {
-//     host: process.env.DBHOSTNAME,
-//     dialect: 'mysql',
-//     port: 26370,
-//     dialectOptions: {
-//         ssl: {
-//             ca: caCert
-//         }
-//     }
-// });
+// Sequelize Configuration
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DBUSERNAME, process.env.DBPASSWORD, {
+    host: process.env.DBHOSTNAME,
+    dialect: 'mysql',
+    port: 26370,
+    dialectOptions: {
+        ssl: {
+            ca: caCert
+        }
+    }
+});
 
 
 
